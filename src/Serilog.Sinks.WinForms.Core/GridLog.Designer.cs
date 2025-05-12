fd35a6170cbfb51469092741de705b8b962171ee
Aleksey Lebedev <aslebedev@gmail.com>
2025-05-12 16:27:02 +0300

    fix autosize

--- src/Serilog.Sinks.WinForms.Core/GridLog.Designer.cs
+++ src/Serilog.Sinks.WinForms.Core/GridLog.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Serilog.Sinks.WinForms.Core
+namespace Serilog.Sinks.WinForms.Core
 {
     partial class GridLog
     {
@@ -28,69 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.LogGridView = new System.Windows.Forms.DataGridView();
-            this.TimeStamp = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Level = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Log = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.LogGridView)).BeginInit();
-            this.SuspendLayout();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            LogGridView = new DataGridView();
+            TimeStamp = new DataGridViewTextBoxColumn();
+            Level = new DataGridViewTextBoxColumn();
+            Log = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)LogGridView).BeginInit();
+            SuspendLayout();
             // 
             // LogGridView
             // 
-            this.LogGridView.AllowUserToAddRows = false;
-            this.LogGridView.AllowUserToDeleteRows = false;
-            this.LogGridView.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.LogGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.LogGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.LogGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.TimeStamp,
-            this.Level,
-            this.Log});
-            this.LogGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LogGridView.Location = new System.Drawing.Point(0, 0);
-            this.LogGridView.Name = "LogGridView";
-            this.LogGridView.ReadOnly = false;
-            this.LogGridView.RowHeadersVisible = false;
-            this.LogGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.LogGridView.Size = new System.Drawing.Size(358, 150);
-            this.LogGridView.TabIndex = 0;
+            LogGridView.AllowUserToAddRows = false;
+            LogGridView.AllowUserToDeleteRows = false;
+            LogGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            LogGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            LogGridView.BackgroundColor = SystemColors.Control;
+            LogGridView.BorderStyle = BorderStyle.Fixed3D;
+            LogGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            LogGridView.Columns.AddRange(new DataGridViewColumn[] { TimeStamp, Level, Log });
+            LogGridView.Dock = DockStyle.Fill;
+            LogGridView.Location = new Point(0, 0);
+            LogGridView.Margin = new Padding(6, 7, 6, 7);
+            LogGridView.Name = "LogGridView";
+            LogGridView.RowHeadersVisible = false;
+            LogGridView.RowHeadersWidth = 62;
+            LogGridView.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            LogGridView.Size = new Size(716, 346);
+            LogGridView.TabIndex = 0;
             // 
             // TimeStamp
             // 
-            this.TimeStamp.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
-            this.TimeStamp.HeaderText = "TimeStamp";
-            this.TimeStamp.Name = "TimeStamp";
-            this.TimeStamp.ReadOnly = true;
-            this.TimeStamp.Width = 85;
+            TimeStamp.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            TimeStamp.HeaderText = "TimeStamp";
+            TimeStamp.MinimumWidth = 8;
+            TimeStamp.Name = "TimeStamp";
+            TimeStamp.Width = 152;
             // 
             // Level
             // 
-            this.Level.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
-            this.Level.HeaderText = "Level";
-            this.Level.Name = "Level";
-            this.Level.ReadOnly = true;
-            this.Level.Width = 58;
+            Level.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            Level.HeaderText = "Level";
+            Level.MinimumWidth = 8;
+            Level.Name = "Level";
+            Level.Width = 96;
             // 
             // Log
             // 
-            this.Log.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.Log.DefaultCellStyle = dataGridViewCellStyle1;
-            this.Log.HeaderText = "Log";
-            this.Log.Name = "Log";
-            this.Log.ReadOnly = true;
+            Log.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            Log.DefaultCellStyle = dataGridViewCellStyle1;
+            Log.HeaderText = "Log";
+            Log.MinimumWidth = 8;
+            Log.Name = "Log";
             // 
             // GridLog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.LogGridView);
-            this.Name = "GridLog";
-            this.Size = new System.Drawing.Size(358, 150);
-            this.Load += new System.EventHandler(this.GridLog_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.LogGridView)).EndInit();
-            this.ResumeLayout(false);
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(LogGridView);
+            Margin = new Padding(6, 7, 6, 7);
+            Name = "GridLog";
+            Size = new Size(716, 346);
+            Load += GridLog_Load;
+            ((System.ComponentModel.ISupportInitialize)LogGridView).EndInit();
+            ResumeLayout(false);
 
         }
 
